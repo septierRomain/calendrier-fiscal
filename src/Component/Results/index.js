df9d7c3c80a0692fc5744a1557a80fbdbe640ac2
Gizmo675 <41105943+Gizmo675@users.noreply.github.com>
2021-12-01 10:17:46 +0100

    adding breadcrumb on results

--- src/Component/Results/index.js
+++ src/Component/Results/index.js
@@ -5,6 +5,9 @@ import TaxContext from '../../Context/TaxContext';
 import VATContext from '../../Context/VATcontext';
 import Banner from '../App/Banner';
 import GreyArrow from '../../Assets/grey arrow.svg'
+import Check from '../../Assets/check.svg'
+
+
 function Results() {
 
   let history = useNavigate()
@@ -41,19 +44,38 @@ function Results() {
     } else if(TAX.isIS) {return(<p>Impot sur les sociétés</p>)}
   }
 
+  const back = () => {
+    history(-1)
+  }
+
   return (
     <div className='wrap'>
       <Banner />
       <div>
-        <button onClick={() =>history(-1)} className="return">
+        <button onClick={() =>back()} className="return">
         <img src={GreyArrow} alt="fleche grise"/>
           Retour
         </button>
-        <h2>votre calendrier fiscal</h2>
-        <div className='breadscrumbs'>
+        <div className='breadcrumbs'>
           <p>Vous etes :</p>
-          {/* <ul>{TAX}</ul> */}
+          {
+            Status.isSAS ? <p><img src={Check} alt="fleche grise"/>une SAS</p> : 
+            Status.isSARL ? <p><img src={Check} alt="fleche grise"/>une SARL/EURL</p> :
+            Status.isIndividual ? <p><img src={Check} alt="fleche grise"/>une entreprise individuelle ou une auto-entreprise</p>:
+            ''
+          }
+          {
+            VAT.isAnnual ? <p><img src={Check} alt="fleche grise"/>au régime de tva simplifié</p> :
+            VAT.isMonthly ? <p><img src={Check} alt="fleche grise"/>au régime mensuel</p> :
+            VAT.isTrimester ? <p><img src={Check} alt="fleche grise"/>au régime trimestrielle</p> :
+            ''
+          }
+          {
+            TAX.isIStrue ? <p><img src={Check} alt="fleche grise"/>à l'impot sur les revenus</p> :
+            <p><img src={Check} alt="fleche grise"/>à l'impot sur les sociétés</p>
+          }
         </div>
+        <h2>votre calendrier fiscal</h2>
         <div>
           <ShowVAT />
           <ShowTAX />
